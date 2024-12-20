f4c320e1452e0d165539cbfcb865f197325329a7
Neil Burningham <kneilb@gmail.com>
2024-12-20 19:12:59 +0000

    Add day3

---
+++ src/bin/day3.rs
@@ -0,0 +1,27 @@
+// https://adventofcode.com/2024/day/3
+
+use regex::Regex;
+
+fn day3(input: &str) -> usize {
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    re.captures_iter(input).map(|c| c.extract()).map(|(_, [a, b])| a.parse::<usize>().unwrap_or(0) * b.parse::<usize>().unwrap_or(0)).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day3_example() {
+	let data = std::fs::read_to_string("data/day3_example.txt").unwrap();
+	let result = day3(&data);
+	assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn day3_real() {
+	let data = std::fs::read_to_string("data/day3.txt").unwrap();
+	let result = day3(&data);
+	assert_eq!(result, 670);
+    }
+}
